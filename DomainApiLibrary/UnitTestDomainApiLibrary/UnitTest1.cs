9040151231c8a1a61ff6c95dbdf3f3ad11637cc6
xdim <xdim2006@yandex.ru>
2015-12-11 01:00:31 +0300

    push

---
+++ DomainApiLibrary/UnitTestDomainApiLibrary/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DomainApiLibrary;
+
+namespace UnitTestDomainApiLibrary
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            var manager = new ManagerOfApi();
+
+            var plugin = manager.StartPlugin("DomainApiLibrary", "DomainApiLibrary.Plugin", "Plugin1");
+            var plugin2 = manager.StartPlugin("DomainApiLibrary", "DomainApiLibrary.Plugin", "Plugin2");
+            Console.WriteLine(plugin.GetDomainName());
+            Console.WriteLine(plugin2.GetDomainName());
+            Console.WriteLine(manager.StopPlugin(plugin));
+            Console.WriteLine(manager.StopPlugin(plugin2));
+        }
+    }
+}
